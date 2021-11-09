59f9fbd31b98758164ed4d5d2f87095e126b4119
Jimmy Bohorquez <ibohorquezjalejandro@gmail.com>
2021-11-08 23:41:19 -0500

    Creacion Activity_1 Version:0.0 (Casa Terracota)

--- app/src/main/java/com/naxozki/greendev/MainActivity.kt
+++ app/src/main/java/com/naxozki/greendev/MainActivity.kt
@@ -3,10 +3,27 @@ package com.naxozki.greendev
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//Importo libreria carousel:
+import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
+import org.imaginativeworld.whynotimagecarousel.ImageCarousel
+
 class MainActivity : AppCompatActivity() {
+
+    //Carousel
+    private val list = mutableListOf<CarouselItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Carousel
+        val carousel: ImageCarousel = findViewById(R.id.carousel)
+        list.add(CarouselItem(imageDrawable = R.drawable.terracota_1, caption = "Terracota"))
+        list.add(CarouselItem(imageDrawable = R.drawable.terracota_2, caption = "Terracota"))
+        list.add(CarouselItem(imageDrawable = R.drawable.terracota_3, caption = "Terracota"))
+        list.add(CarouselItem(imageDrawable = R.drawable.terracota_4, caption = "Terracota"))
+        list.add(CarouselItem(imageDrawable = R.drawable.terracota_5, caption = "Terracota"))
+        carousel.addData(list)
     }
 }
-//Comentario para crear la rama development
